0b00a54a9d93fe67fa9f724b7ae11c46856971a6
Tom Deckers <tdeckers@cisco.com>
2019-07-30 01:45:28 +0200

    Add collision detection and car bounding box.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Overview
 
+Inspired by: https://www.youtube.com/watch?v=r428O_CMcpI
+
 ## Initial project setup
 
 git init
@@ -10,3 +12,13 @@ python -m venv venv
 ## Update / reset project
 
 pip install -r .\requirements.txt
+
+## Reference
+
+Collision detection: http://jeffreythompson.org/collision-detection/line-point.php
+
+## TODO
+
+Consider using pymunk physics engine for motion. Example:
+https://github.com/slembcke/Chipmunk2D/blob/master/demo/Tank.c
+Right now, only using pymunk for vector calc.
--- hello_world.py
+++ hello_world.py
@@ -1,22 +1,42 @@
 import pyglet
 from pyglet.window import key, mouse
-from math import tan, radians, degrees, copysign, floor
+from math import tan, radians, degrees, copysign, floor, cos, sin
 import builtins
-from pymunk import Vec2d
+from pymunk import Vec2d, Poly
+from itertools import chain
 
 window = pyglet.window.Window(800, 600,caption="Game")
 keymap = pyglet.window.key.KeyStateHandler()
 window.push_handlers(keymap)
 batch = pyglet.graphics.Batch()
+car_batch = pyglet.graphics.Batch()
 fps_display = pyglet.window.FPSDisplay(window=window)
+
+# https://stackoverflow.com/questions/34372480/rotate-point-about-another-point-in-degrees-python
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+
+    The angle should be given in degrees.
+    """
+    ox, oy = origin
+    px, py = point
+
+    angle_r = radians(angle)
+    qx = ox + cos(angle_r) * (px - ox) - sin(angle_r) * (py - oy)
+    qy = oy + sin(angle_r) * (px - ox) + cos(angle_r) * (py - oy)
+    return qx, qy
 
 class Track:
     def __init__(self):
         #group = pyglet.graphics.Group()
-        self.outer_points = [112, 542, 315, 541, 476, 539, 609, 544, 700, 514, 721, 451, 734, 323, 736, 172, 692, 95, 628, 75, 514, 64, 445, 72, 440, 122, 414, 177, 404, 246, 345, 254, 321, 242, 301, 186, 290, 147, 276, 86, 250, 41, 166, 57, 78, 75, 48, 205, 29, 396, 47, 503]
-        batch.add(int(len(self.outer_points) / 2), pyglet.gl.GL_LINE_LOOP, None, ('v2i', tuple(self.outer_points)))
-        self.inner_points = [139, 472, 357, 462, 473, 456, 562, 464, 621, 453, 639, 422, 642, 386, 646, 312, 657, 263, 647, 188, 623, 155, 565, 149, 524, 148, 501, 192, 485, 244, 477, 298, 433, 327, 379, 332, 317, 335, 261, 309, 250, 265, 219, 220, 213, 180, 193, 138, 172, 134, 153, 149, 143, 210, 121, 372, 121, 451]
+        self.outer_points = [112, 542, 315, 541, 476, 539, 609, 544, 700, 514, 721, 451, 734, 323, 736, 172, 692, 95, 628, 75, 514, 64, 445, 72, 440, 122, 414, 177, 404, 246, 345, 254, 321, 242, 301, 186, 290, 147, 276, 86, 250, 41, 166, 57, 78, 75, 48, 205, 29, 396, 47, 503, 112, 542]
+        batch.add(int(len(self.outer_points) / 2), pyglet.gl.GL_LINE_STRIP, None, ('v2i', tuple(self.outer_points)))
+        self.inner_points = [139, 472, 357, 462, 473, 456, 562, 464, 621, 453, 639, 422, 642, 386, 646, 312, 657, 263, 647, 188, 623, 155, 565, 149, 524, 148, 501, 192, 485, 244, 477, 298, 433, 327, 379, 332, 317, 335, 261, 309, 250, 265, 219, 220, 213, 180, 193, 138, 172, 134, 153, 149, 143, 210, 121, 372, 121, 451, 139, 472] 
         batch.add(int(len(self.inner_points) / 2), pyglet.gl.GL_LINE_LOOP, None, ('v2i', tuple(self.inner_points)))
+
+    def get_lines(self):
+        return
 
 class Car(pyglet.sprite.Sprite):            
     def __init__(self, x, y, angle=0.0, max_steering=40, max_acceleration=350.0):
@@ -24,7 +44,7 @@ class Car(pyglet.sprite.Sprite):
         image = pyglet.image.load('resources/car.png')
         image.anchor_x = int(image.width / 2)
         image.anchor_y = int(image.height / 2)
-        pyglet.sprite.Sprite.__init__(self, image, x, y, batch=batch)  
+        pyglet.sprite.Sprite.__init__(self, image, x, y, batch=car_batch)
         self.scale = 0.4
         self.position_vector = Vec2d(x, y)
         self.velocity = Vec2d(0.0, 0.0)
@@ -39,6 +59,7 @@ class Car(pyglet.sprite.Sprite):
 
         self.acceleration = 0.0
         self.steering = 0.0
+        self.shape = car_batch.add(4, pyglet.gl.GL_LINE_LOOP, None, ('v2i', self.get_body()))        
 
     def handle_player(self, dt):
         if keymap[key.UP]:
@@ -51,7 +72,7 @@ class Car(pyglet.sprite.Sprite):
             if self.velocity.x > 0:
                 self.acceleration = -self.brake_deceleration
             else:
-                self.acceleration -= 200 * dt
+                self.acceleration = -self.max_acceleration # Pedal = metal
         elif keymap[key.SPACE]:
             if abs(self.velocity.x) > dt * self.brake_deceleration:
                 self.acceleration = -copysign(self.brake_deceleration,self.velocity.x)
@@ -62,10 +83,10 @@ class Car(pyglet.sprite.Sprite):
                 self.acceleration = -copysign(self.free_deceleration, self.velocity.x)
             else:
                 if dt != 0:
-                    self.acceleration = -self.velocity.x / dt    
+                    self.acceleration = -1 * self.velocity.x / dt    
         
         # limit max accelleration.   
-        if not keymap[key.SPACE]:     
+        if not keymap[key.SPACE]: # except when braking. 
             self.acceleration = max(-self.max_acceleration, min(self.acceleration, self.max_acceleration))
 
         if keymap[key.RIGHT]:
@@ -101,13 +122,25 @@ class Car(pyglet.sprite.Sprite):
         self.x = self.position_vector.x
         self.y = self.position_vector.y
 
+        self.shape.vertices = self.get_body()
+
+    # https://stackoverflow.com/questions/12161277/how-to-rotate-a-vertex-around-a-certain-point
+    # for each corner, apply rotation around (x,y) 
+    def get_body(self):
+        p1 = (self.x - self.width/2, self.y - self.height/2)
+        p2 = (self.x - self.width/2, self.y + self.height/2)
+        p3 = (self.x + self.width/2, self.y + self.height/2)
+        p4 = (self.x + self.width/2, self.y - self.height/2)
+        center = self.position_vector.int_tuple
+        q1 = rotate(center, p1, -self.rotation)
+        q2 = rotate(center, p2, -self.rotation)
+        q3 = rotate(center, p3, -self.rotation)
+        q4 = rotate(center, p4, -self.rotation)
+        body_tuple = tuple(chain(*(q1, q2, q3, q4))) # flatten
+        return tuple(int(i) for i in body_tuple) # convert floats to ints
+
     def __repr__(self):
         return f"Car(pos={self.position_vector}, ang={self.angle})"
-
-class Debug(pyglet.text.document.FormattedDocument):    
-    def __init__(game):
-        pyglet.text.FormattedDocument.__init__()
-
 
 class Gamestate():
     def __init__(self):
@@ -115,10 +148,8 @@ class Gamestate():
         self.track = Track()
         self.debug_sprite = pyglet.text.Label('Sprite debug', font_size=10, font_name='Times New Roman',
                           x=790, y=590, anchor_x='right', anchor_y='center', batch=batch)
-        self.debug_body = pyglet.text.Label('Body debug', font_size=10, font_name='Times New Roman',
-                          x=790, y=575, anchor_x='right', anchor_y='center', batch=batch)
         self.debug_vel = pyglet.text.Label('Vel debug', font_size=10, font_name='Times New Roman',
-                          x=790, y=560, anchor_x='right', anchor_y='center', batch=batch)                          
+                          x=790, y=575, anchor_x='right', anchor_y='center', batch=batch)                          
     
     def handle_player(self, dt):
         self.car.handle_player(dt)
@@ -127,14 +158,58 @@ class Gamestate():
         self.debug_sprite.text = f"sprite x: {floor(self.car.x)}, y: {floor(self.car.y)}, dir: {floor(self.car.rotation)}"
         self.debug_vel.text = f"l: {round(self.car.velocity.x,1)}, dir: {floor(self.car.velocity.angle)}, accel: {round(self.car.acceleration, 1)}"
         self.handle_player(dt)
-
+    
 world = Gamestate()        
+
+accuracy = 0.1
+
+# detect line/point collision
+def line_point_hit(a, b, p):
+    l = a.get_distance(b)
+    d1 = a.get_distance(p)
+    d2 = b.get_distance(p)
+    if (d1+d2 >= l-accuracy and d1+d2 <= l+accuracy):
+        return True
+    return False
+
+def line_line_hit(a, b, c, d):
+    uA = ((d.x-c.x)*(a.y-c.y) - (d.y-c.y)*(a.x-c.x)) / ((d.y-c.y)*(b.x-a.x) - (d.x-c.x)*(b.y-a.y))
+    uB = ((b.x-a.x)*(a.y-c.y) - (b.y-a.y)*(a.x-c.x)) / ((d.y-c.y)*(b.x-a.x) - (d.x-c.x)*(b.y-a.y))
+    if (uA >= 0 and uA <= 1 and uB >= 0 and uB <= 1):
+        intersectionX = a.x + (uA * (b.x-a.x))
+        intersectionY = a.y + (uA * (b.y-a.y))
+        return Vec2d(intersectionX, intersectionY)
+    return None
 
 @window.event
 def on_draw():
     window.clear()
     fps_display.draw() 
     batch.draw()
+    car_batch.draw()
+
+hit_point_v = None
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    global hit_point_v
+    hit_point = line_line_hit(Vec2d(112, 542), Vec2d(315, 541), Vec2d(0,0), Vec2d(x,y))
+    if hit_point != None:
+        hit = [hit_point.x - 5, hit_point.y - 5,
+                                hit_point.x + 5, hit_point.y + 5,
+                                hit_point.x - 5, hit_point.y + 5,
+                                hit_point.x + 5, hit_point.y - 5]
+        if hit_point_v != None:
+            hit_point_v.delete()
+        hit_point_v = batch.add(4, pyglet.gl.GL_LINES, None, 
+            ('v2i', [int(i) for i in hit]),
+            ('c3B', (255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0,))
+        )        
+    else:
+        if hit_point_v != None:
+            hit_point_v.delete()
+            hit_point_v = None
+
 
 def update(dt):
     world.update(dt)
@@ -147,19 +222,11 @@ def start_up():
     pyglet.app.run()
 
     # @window.event
-    # def on_key_release(symbol, modifiers):
-    #     if (symbol == key.RIGHT) or (symbol == key.LEFT):
-    #         car.turn(0)
-    #     elif (symbol == key.UP) or (symbol == key.DOWN):
-    #         car.accel(0)
-
-    # @window.event
     # def on_mouse_press(x, y, button, modifiers):
     #     if button == mouse.LEFT:
     #         print(f"x={x}, y={y}")
     #         track.inner_points.append(x)
     #         track.inner_points.append(y)
-
 
 if __name__ == '__main__':
     start_up()
